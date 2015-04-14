6ff229f548af384f82419ab40a4dfd09a8d76a4b
Alexandra Svyatova <alexsvyatova@gmail.com>
2015-04-14 12:28:16 +0600

    Create fl021.cpp

---
+++ fl021.cpp
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////
+// fl02.cpp
+// Окошко с кнопкой и обработчиком событий
+//
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <stdlib.h>
+
+void fbt(Fl_Widget *w, void *) {//point out our button
+	
+	if (w->color()==FL_RED) {
+	    w->color(FL_YELLOW);
+		return ;	
+   }
+   if (w->color()==FL_YELLOW) {
+		w->color(FL_GREEN);
+		return ;
+   }
+   if (w->color()==FL_GREEN) {
+	  w->color(FL_RED); 
+      return ;
+   }
+
+}
+
+int main(){
+   Fl_Window win(300, 200, "TEST");      // создаем окно
+   Fl_Button bt(10, 10, 80, 30, "&bt");
+   bt.color(FL_RED);
+   bt.callback(fbt,0);                   // подключаем обработчик события нажатия на кнопку
+   win.show();                           // отображаем окно
+   return Fl::run();                     // запускаем обработчик событий
+}
+// end of file fl02.cpp
+////////////////////////////////////////////////////////////////////////////
